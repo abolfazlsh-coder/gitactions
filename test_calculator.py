a1b5224c512ee7354661628aa23fe9b515d08260
Abolfazl Shishegar <104524823+abolfazlsh-coder@users.noreply.github.com>
2023-06-05 22:39:46 +0330

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,23 +1,33 @@
 import unittest
-
 import calculator
 
 
 class Testcase(unittest.TestCase):
+
     def test_add(self):
         self.assertEqual(calculator.add(2, 3), 5)
+        self.assertEqual(calculator.add(2, -3), -1)
+        self.assertEqual(calculator.add(-2, -3), -5)
+        self.assertEqual(calculator.add(2, 0), 2)
 
     def test_subtract(self):
         self.assertEqual(calculator.subtract(3, 2), 1)
+        self.assertEqual(calculator.subtract(3, -2), 5)
+        self.assertEqual(calculator.subtract(-3, -2), -1)
+        self.assertEqual(calculator.subtract(3, 0), 3)
 
     def test_multiply(self):
         self.assertEqual(calculator.multiply(2, 3), 6)
+        self.assertEqual(calculator.multiply(2, -3), -6)
+        self.assertEqual(calculator.multiply(-2, -3), 6)
+        self.assertEqual(calculator.multiply(2, 0), 0)
 
     def test_divide(self):
         self.assertEqual(calculator.divide(6, 3), 2)
-        with self.assertRaises(ValueError):
-            calculator.divide(6, 0)
+        self.assertEqual(calculator.divide(6, -3), -2)
+        self.assertEqual(calculator.divide(-6, -3), 2)
+        self.assertEqual(calculator.divide(6, 1), 6)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
