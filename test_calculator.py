53007c7ca0a2e98ef9228499086d4135905ca473
Abolfazl Shishegar <104524823+abolfazlsh-coder@users.noreply.github.com>
2023-06-05 19:51:02 +0330

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+import unittest
+import calculator
+class Testcase(unittest.TestCase):
+    
+	def test_add(self):
+		self.assertEqual(calculator.add(2,3),5)
+	def test_subtract(self):
+		self.assertEqual(calculator.subtract(3,2),1)
+	def test_multiply(self):
+		self.assertEqual(calculator.multiply(2,3),6)
+	def test_divide(self):
+		self.assertEqual(calculator.divide(6,3),2)
+		with self.assertRaises(ValueError):
+			calculator.divide(6,0)
+
+if __name__ == '__main__':
+	unittest.main()
